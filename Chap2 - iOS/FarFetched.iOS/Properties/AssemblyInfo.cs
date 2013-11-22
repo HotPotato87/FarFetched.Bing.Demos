2220dd8ea3bc2661be0752bbdac76c1f15767f72
HotPotato87 <patrick.mccurley@gmail.com>
2013-11-22 14:14:38 +0000

    folder restruct

--- Chap2 - iOS/FarFetched.iOS/Properties/AssemblyInfo.cs
+++ Chap2 - iOS/FarFetched.iOS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("89969ccc-53c7-42cc-9874-97a08c89b889")]
+[assembly: Guid("134b60cf-773c-484b-a365-05e8b58dc84c")]
 
 // Version information for an assembly consists of the following four values:
 //
